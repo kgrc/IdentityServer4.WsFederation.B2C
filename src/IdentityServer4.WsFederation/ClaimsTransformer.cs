50f39076d99d309bca274be3a422560f444caca1
Ratheesh C Krishnageeth <ratheeshc@ilink-systems.com>
2020-08-21 12:46:48 -0400

    Revert "Revert ""Sub Claim Missing Issue resolved"""
    This reverts commit a999ee5016da3e801d8bb9ecf6e5cc2d971510c9.

---
+++ src/IdentityServer4.WsFederation/ClaimsTransformer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace IdentityServer4.WsFederation
+{
+    public class ClaimsTransformer : IClaimsTransformation
+    {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            // This will run every time Authenticate is called so its better to create a new Principal
+            //Ratheesh: Just adding a custom claim 'timestamp' to the outgoing claim
+            //Edit this to add more claims as RP needs.
+            var transformed = new ClaimsPrincipal();
+            transformed.AddIdentities(principal.Identities);
+            transformed.AddIdentity(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("Timestamp", DateTime.Now.ToString())
+            }));
+            return Task.FromResult(transformed);
+        }
+    }
+}
--- src/IdentityServer4.WsFederation/Config.cs
+++ src/IdentityServer4.WsFederation/Config.cs
@@ -2,6 +2,9 @@
 using IdentityServer4.Models;
 using System.Collections.Generic;
 using static IdentityServer4.IdentityServerConstants;
+using IdentityServer4.WsFederation.Stores;
+using System.IdentityModel.Claims;
+using System.IdentityModel.Tokens;
 
 namespace IdentityServer4.WsFederation
 {
@@ -52,6 +55,33 @@ namespace IdentityServer4.WsFederation
                     AllowedScopes = { "openid", "profile" }
                 }
             };
+        }
+
+        public static List<RelyingParty> GetRelyingParties()
+        {
+            return new List<RelyingParty> {
+            new RelyingParty {
+                // Same as ClientId. Used to link config
+                Realm = "urn:aspnetcorerp",
+
+                // SAML 1.1 token type required by SharePoint
+                TokenType = WsFederationConstants.TokenTypes.Saml11TokenProfile11,
+
+                // Transform claim types from oidc standard to xml types
+                // Only mapped claims will be returned for SAML 1.1 tokens
+                ClaimMapping = new Dictionary<string, string> {
+                    {JwtClaimTypes.Subject, ClaimTypes.NameIdentifier},
+                    {JwtClaimTypes.Name, ClaimTypes.Name},
+                    {JwtClaimTypes.Email, ClaimTypes.Email},
+                    {JwtClaimTypes.GivenName, ClaimTypes.GivenName}
+                },
+
+                // Defaults
+                DigestAlgorithm = SecurityAlgorithms.Sha256Digest,
+                SignatureAlgorithm = SecurityAlgorithms.RsaSha256Signature,
+                SamlNameIdentifierFormat = WsFederationConstants.SamlNameIdentifierFormats.UnspecifiedString
+            }
+        };
         }
     }
 }
